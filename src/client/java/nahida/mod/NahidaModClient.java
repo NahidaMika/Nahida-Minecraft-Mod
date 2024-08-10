8c2125f9d1715befb73ba4682487d2dae51f7cfe
Nahida <51387274+NahidaMika@users.noreply.github.com>
2024-08-10 15:08:12 -0600

    Created repositry

---
+++ src/client/java/nahida/mod/NahidaModClient.java
@@ -0,0 +1,10 @@
+package nahida.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NahidaModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/nahida/mod/NahidaModDataGenerator.java
@@ -0,0 +1,11 @@
+package nahida.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NahidaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
